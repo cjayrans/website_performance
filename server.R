8dacf2656ec7a2561ce3d2c9e6fb33941d630ddf
cjayrans <46723535+cjayrans@users.noreply.github.com>
2019-03-01 19:25:29 -0500

    Update 'organic_summ_sales2' reactive value to convert values to appropriate formats
    Updated number formats are; numerical with commas, dollar, and percentage for display purposes

--- server.R
+++ server.R
@@ -118,9 +118,19 @@ shinyServer(function(input, output) {
   # Gathered summary for the Page Specific sales
   organic_summ_sales2 <- reactive({
     
-    organic_summ_page_specific[(organic_summ_page_specific$site_page == input$pageFilterSales),
+    org_summ_sales_temp2 <- organic_summ_page_specific[(organic_summ_page_specific$site_page == input$pageFilterSales),
                                c('month','site_page','buys','total_revenue','gross_spread','gross_spread_perc','spread_per_unit','cost_per_unit','salePrice_per_unit')]
     
+    org_summ_num_temp2 <- prettyNum(org_summ_sales_temp2$buys, big.mark=",")
+    
+    org_summ_sales_dollars2 <- as.data.frame(apply(org_summ_sales_temp2[,
+                                                                        c('total_revenue','gross_spread','spread_per_unit','cost_per_unit','salePrice_per_unit')],
+                                                                                                                                     2, function(x) dollar_format()(x)))
+                                                   
+    org_summ_sales_perc2 <- as.data.frame(lapply(org_summ_sales_temp2[, c('gross_spread_perc')], function(x) percent(x)))
+    
+    # cbind(org_summ_sales_temp2[,c('Monthly','buy_count','lots_sold')], org_summ_sales_temp3, org_summ_sales_temp2[,c('spread_perc','success_rate')])
+    cbind(org_summ_sales_temp2[,c('month','site_page')], org_summ_num_temp2, org_summ_sales_dollars2, org_summ_sales_perc2)
     
   })
   
